698df9bf30907af6ccf933f62724f5f3013c7c6b
Edosikkk <snezhachernova85@gmail.com>
2024-10-03 21:25:21 +0300

    fe

---
+++ client/pages/[id].vue
@@ -0,0 +1,9 @@
+<template>
+    <nav>
+        <ul>
+            <li><nuxtlink to="/blog">Блог</nuxtlink></li>
+            <li><nuxtlink to="/category/"></nuxtlink></li>
+            <li><strong>{{  }}</strong></li>
+        </ul>
+    </nav>
+</template>
---
+++ client/pages/blog.vue
@@ -0,0 +1,7 @@
+<template>
+    <h2>Блог</h2>
+</template>
+
+<main>
+    
+</main>
